e2816add94e719766f28535ea9d5b8a33ed9effd
mhji <battlesun99@gmail.com>
2021-08-09 16:37:54 +0900

    Feat: create useScroll using by useEffect

---
+++ src/components/useEffect/useScroll.js
@@ -0,0 +1,31 @@
+import { useEffect, useState, Fragment } from 'react';
+
+const useScroll = () => {
+  const [state, setState] = useState({
+    x: 0,
+    y: 0
+  });
+
+  const onScroll = () => {
+    setState({y: window.scrollY, x: window.scrollX});
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  })
+
+  return state;
+};
+
+function Scroll() {
+  const { y } = useScroll();
+  return (
+    <Fragment style={{height: "1000vh"}}>
+      <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>Hi</h1>
+    </Fragment>
+  );
+}
+
+export default Scroll;
